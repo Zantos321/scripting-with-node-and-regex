2077350ac7ea78dada1589058351aae66f11ae5f
149203 <149203@gmail.com>
2020-05-21 16:01:44 -0700

    update trim desc

--- regex.js
+++ regex.js
@@ -12,6 +12,6 @@ module.exports = {
       return "use the string .match() method and regex to find what you are looking for"
    },
    trim(str) {
-      return "use the string .match() method and regex to find what you are looking for"
+      return "use the string .replace() method and regex to first replace carriage returns and new lines with a space, then replace 2 or more spaces with 1 space, then remove spaces from beginning and end"
    },
 };
